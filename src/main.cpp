85ad106221f4d6b54a6ca6832d9ee3692c7899c9
Ian <ianjrigg@gmail.com>
2019-12-15 10:32:32 -0600

    Remove cube rendering functions from the main

--- src/main.cpp
+++ src/main.cpp
@@ -209,332 +209,6 @@ static void setup_signal_handlers()
 #include <cmath>
 #include <iomanip>
 
-const std::vector<float> cube_vertices_with_duplicates
-{
-//    Positions               Colors                 Texture coords
-      -0.5f, -0.5f, -0.5f,    0.0f,  0.0f,  0.0f,    0.0f, 0.0f,
-       0.5f,  0.5f, -0.5f,    0.0f,  0.0f,  0.0f,    1.0f, 1.0f,
-       0.5f, -0.5f, -0.5f,    0.0f,  0.0f,  0.0f,    1.0f, 0.0f,
-       0.5f,  0.5f, -0.5f,    0.0f,  0.0f,  0.0f,    1.0f, 1.0f,
-      -0.5f, -0.5f, -0.5f,    0.0f,  0.0f,  0.0f,    0.0f, 0.0f,
-      -0.5f,  0.5f, -0.5f,    0.0f,  0.0f,  0.0f,    0.0f, 1.0f,
-
-      -0.5f, -0.5f,  0.5f,    0.0f,  0.0f,  0.0f,    0.0f, 0.0f,
-       0.5f, -0.5f,  0.5f,    0.0f,  0.0f,  0.0f,    1.0f, 0.0f,
-       0.5f,  0.5f,  0.5f,    0.0f,  0.0f,  0.0f,    1.0f, 1.0f,
-       0.5f,  0.5f,  0.5f,    0.0f,  0.0f,  0.0f,    1.0f, 1.0f,
-      -0.5f,  0.5f,  0.5f,    0.0f,  0.0f,  0.0f,    0.0f, 1.0f,
-      -0.5f, -0.5f,  0.5f,    0.0f,  0.0f,  0.0f,    0.0f, 0.0f,
-
-      -0.5f,  0.5f,  0.5f,    0.0f,  0.0f,  0.0f,    1.0f, 0.0f,
-      -0.5f,  0.5f, -0.5f,    0.0f,  0.0f,  0.0f,    1.0f, 1.0f,
-      -0.5f, -0.5f, -0.5f,    0.0f,  0.0f,  0.0f,    0.0f, 1.0f,
-      -0.5f, -0.5f, -0.5f,    0.0f,  0.0f,  0.0f,    0.0f, 1.0f,
-      -0.5f, -0.5f,  0.5f,    0.0f,  0.0f,  0.0f,    0.0f, 0.0f,
-      -0.5f,  0.5f,  0.5f,    0.0f,  0.0f,  0.0f,    1.0f, 0.0f,
-
-       0.5f,  0.5f,  0.5f,    0.0f,  0.0f,  0.0f,    1.0f, 0.0f,
-       0.5f, -0.5f, -0.5f,    0.0f,  0.0f,  0.0f,    0.0f, 1.0f,
-       0.5f,  0.5f, -0.5f,    0.0f,  0.0f,  0.0f,    1.0f, 1.0f,
-       0.5f, -0.5f, -0.5f,    0.0f,  0.0f,  0.0f,    0.0f, 1.0f,
-       0.5f,  0.5f,  0.5f,    0.0f,  0.0f,  0.0f,    1.0f, 0.0f,
-       0.5f, -0.5f,  0.5f,    0.0f,  0.0f,  0.0f,    0.0f, 0.0f,
-
-      -0.5f, -0.5f, -0.5f,    0.0f,  0.0f,  0.0f,    0.0f, 1.0f,
-       0.5f, -0.5f, -0.5f,    0.0f,  0.0f,  0.0f,    1.0f, 1.0f,
-       0.5f, -0.5f,  0.5f,    0.0f,  0.0f,  0.0f,    1.0f, 0.0f,
-       0.5f, -0.5f,  0.5f,    0.0f,  0.0f,  0.0f,    1.0f, 0.0f,
-      -0.5f, -0.5f,  0.5f,    0.0f,  0.0f,  0.0f,    0.0f, 0.0f,
-      -0.5f, -0.5f, -0.5f,    0.0f,  0.0f,  0.0f,    0.0f, 1.0f,
-
-      -0.5f,  0.5f, -0.5f,    0.0f,  0.0f,  0.0f,    0.0f, 1.0f,
-       0.5f,  0.5f,  0.5f,    0.0f,  0.0f,  0.0f,    1.0f, 0.0f,
-       0.5f,  0.5f, -0.5f,    0.0f,  0.0f,  0.0f,    1.0f, 1.0f,
-       0.5f,  0.5f,  0.5f,    0.0f,  0.0f,  0.0f,    1.0f, 0.0f,
-      -0.5f,  0.5f, -0.5f,    0.0f,  0.0f,  0.0f,    0.0f, 1.0f,
-      -0.5f,  0.5f,  0.5f,    0.0f,  0.0f,  0.0f,    0.0f, 0.0f
-};
-
-const std::vector<float> cube_vertices_without_params
-{
-//    Positions
-       0.0f,  0.0f,  0.0f,  // Back Side, bottom right
-       1.0f,  0.0f,  0.0f,  // Back Side, bottom left
-       0.0f,  1.0f,  0.0f,  // Back Side, top right
-       1.0f,  1.0f,  0.0f,  // Back Side, top left
-
-       0.0f,  0.0f,  1.0f,  // Front Side, bottom left
-       1.0f,  0.0f,  1.0f,  // Front Side, bottom right
-       0.0f,  1.0f,  1.0f,  // Front Side, top left
-       1.0f,  1.0f,  1.0f,  // Front Side, top right
-
-       0.0f,  0.0f,  0.0f,  // Left Side, bottom left
-       0.0f,  0.0f,  1.0f,  // Left Side, bottom right
-       0.0f,  1.0f,  1.0f,  // Left Side, top right
-       0.0f,  1.0f,  0.0f,  // Left Side, top left
-
-       1.0f,  0.0f,  1.0f,  // Right Side, bottom left
-       1.0f,  0.0f,  0.0f,  // Right Side, bottom right
-       1.0f,  1.0f,  0.0f,  // Right Side, top right
-       1.0f,  1.0f,  1.0f,  // Right Side, top left
-
-       0.0f,  0.0f,  0.0f,  // Bottom Side, bottom left
-       1.0f,  0.0f,  0.0f,  // Bottom Side, bottom right
-       1.0f,  0.0f,  1.0f,  // Bottom Side, top right
-       0.0f,  0.0f,  1.0f,  // Bottom Side, top left
-
-       0.0f,  1.0f,  1.0f,  // Top Side, bottom left
-       1.0f,  1.0f,  1.0f,  // Top Side, bottom right
-       1.0f,  1.0f,  0.0f,  // Top Side, top right
-       0.0f,  1.0f,  0.0f  // Top Side, top left
-};
-
-const std::vector<float> cube_vertices
-{
-//    Positions               Texture coords
-       0.0f,  0.0f,  0.0f,    0.0f, 0.0f, // Back Side, bottom right
-       1.0f,  0.0f,  0.0f,    1.0f, 0.0f, // Back Side, bottom left
-       0.0f,  1.0f,  0.0f,    0.0f, 1.0f, // Back Side, top right
-       1.0f,  1.0f,  0.0f,    1.0f, 1.0f, // Back Side, top left
-
-       0.0f,  0.0f,  1.0f,    0.0f, 0.0f, // Front Side, bottom left
-       1.0f,  0.0f,  1.0f,    1.0f, 0.0f, // Front Side, bottom right
-       0.0f,  1.0f,  1.0f,    0.0f, 1.0f, // Front Side, top left
-       1.0f,  1.0f,  1.0f,    1.0f, 1.0f, // Front Side, top right
-
-       0.0f,  0.0f,  0.0f,    0.0f, 1.0f, // Left Side, bottom left
-       0.0f,  0.0f,  1.0f,    0.0f, 0.0f, // Left Side, bottom right
-       0.0f,  1.0f,  1.0f,    1.0f, 0.0f, // Left Side, top right
-       0.0f,  1.0f,  0.0f,    1.0f, 1.0f, // Left Side, top left
-
-       1.0f,  0.0f,  1.0f,    0.0f, 0.0f, // Right Side, bottom left
-       1.0f,  0.0f,  0.0f,    0.0f, 1.0f, // Right Side, bottom right
-       1.0f,  1.0f,  0.0f,    1.0f, 1.0f, // Right Side, top right
-       1.0f,  1.0f,  1.0f,    1.0f, 0.0f, // Right Side, top left
-
-       0.0f,  0.0f,  0.0f,    0.0f, 1.0f, // Bottom Side, bottom left
-       1.0f,  0.0f,  0.0f,    1.0f, 1.0f, // Bottom Side, bottom right
-       1.0f,  0.0f,  1.0f,    1.0f, 0.0f, // Bottom Side, top right
-       0.0f,  0.0f,  1.0f,    0.0f, 0.0f, // Bottom Side, top left
-
-       0.0f,  1.0f,  1.0f,    0.0f, 0.0f, // Top Side, bottom left
-       1.0f,  1.0f,  1.0f,    1.0f, 0.0f, // Top Side, bottom right
-       1.0f,  1.0f,  0.0f,    1.0f, 1.0f, // Top Side, top right
-       0.0f,  1.0f,  0.0f,    0.0f, 1.0f  // Top Side, top left
-};
-
-std::vector<uint32_t> cube_indices
-{
-    1, 0, 2,
-    1, 2, 3,
-
-    4, 5, 7,
-    4, 7, 6,
-
-    8, 9, 10,
-    8, 10, 11,
-
-    12, 13, 14,
-    12, 14, 15,
-
-    16, 17, 18,
-    16, 18, 19,
-
-    20, 21, 22,
-    20, 22, 23
-};
-
-void render_cube()
-{
-    Window window("Render Cube", 768UL, 1280UL);
-    window.enable_vsync();
-    window.enable_depth_test();
-    window.enable_wireframe();
-    window.enable_backface_culling();
-
-    // IMGUI_CHECKVERSION();
-    ImGui::CreateContext();
-    ImGuiIO& io = ImGui::GetIO();
-    io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;     // Enable Keyboard Controls
-
-    // // Setup Dear ImGui style
-    ImGui::StyleColorsDark();
-
-    // Setup Platform/Renderer bindings
-    window.register_imgui();
-    ImGui_ImplOpenGL3_Init("#version 410 core");
-
-    Mesh mesh;
-    mesh.bind();
-    mesh.load_vertex_buffer(cube_vertices_without_params);
-    mesh.load_index_buffer(cube_indices);
-    mesh.set_vertex_buffer_attribute(0, 3, 0);
-
-    // Texture texture;
-    // texture.bind_with_2D_texture_as_target();
-    // texture.load_jpg("textures/container.jpg");
-    // texture.set_s_axis_to_repeat();
-    // texture.set_t_axis_to_repeat();
-    // texture.set_minifying_filter_to_nearest();
-    // texture.set_magnifying_filter_to_nearest();
-
-    Program program;
-    program.load_vertex_shader("shaders/vertex/cube.vert");
-    program.load_fragment_shader("shaders/fragment/cube.frag");
-    program.link();
-
-    program.use();
-
-    //program.set_uniform_integer("texture1", 0);
-
-    Camera camera;
-    camera.position.z = 2.0f;
-
-
-    glm::vec3 x_axis(1.0f, 0.0f, 0.0f);
-    glm::vec3 y_axis(0.0f, 1.0f, 0.0f);
-    glm::vec3 rotation_axis(0.0f, 0.0f, 0.0f);
-
-    glm::mat4 transformation(1.0f);
-    glm::mat4 rotation(1.0f);
-    glm::mat4 scale(1.0f);
-
-    glm::mat4 model(1.0f);
-    glm::mat4 view = camera.look_at();
-    glm::mat4 projection = glm::perspective(glm::radians(camera.fov()), window.aspect_ratio(), 0.1f, 100.0f);
-
-    glm::mat4 mvp(1.0f);
-
-    bool mouse_is_being_held_down = false;
-
-    double start_x_position = 0.0f;
-    double current_x_position = 0.0f;
-
-    double start_y_position = 0.0f;
-    double current_y_position = 0.0f;
-
-    glm::vec3 cube_angles(0.0f, 0.0f, 0.0f);
-
-    while(no_signals_have_been_raised() && window.should_not_close())
-    {
-        // IO Logic
-        window.poll_input();
-
-        // Make sure tha the mouse events aren't happneing in the UI
-        if(io.WantCaptureMouse == false)
-        {
-            if(window.left_mouse_button_is_pressed())
-            {
-                if(mouse_is_being_held_down)
-                {
-                    // Take care of x rotations
-                    start_x_position = current_x_position;
-                    current_x_position = window.mouse_x_position();
-
-                    if(current_x_position - start_x_position >= 2.0f)
-                    {
-                        cube_angles.x +=  1.0f;
-                        rotation = glm::rotate(rotation, glm::radians(1.0f), y_axis);
-                    }
-                    else if(current_x_position - start_x_position <= -2.0f)
-                    {
-                        cube_angles.x += -1.0f;
-                        rotation = glm::rotate(rotation, glm::radians(-1.0f), y_axis);
-                    }
-
-                    if(cube_angles.x >= 360.0f || cube_angles.x <= -360.0f)
-                    {
-                        cube_angles.x = 0.0f;
-                    }
-
-                    // Take care of y rotations
-                    start_y_position = current_y_position;
-                    current_y_position = window.mouse_y_position();
-
-                    if(current_y_position - start_y_position >= 2.0f)
-                    {
-                        cube_angles.y +=  1.0f;
-                        rotation = glm::rotate(rotation, glm::radians(1.0f), x_axis);
-                    }
-                    else if(current_y_position - start_y_position <= -2.0f)
-                    {
-                        cube_angles.y += -1.0f;
-                        rotation = glm::rotate(rotation, glm::radians(-1.0f), x_axis);
-                    }
-
-                    if(cube_angles.y >= 360.0f || cube_angles.y <= -360.0f)
-                    {
-                        cube_angles.y = 0.0f;
-                    }
-                }
-                else
-                {
-                    start_x_position = window.mouse_y_position();
-                    mouse_is_being_held_down = true;
-                }
-            }
-            else
-            {
-                mouse_is_being_held_down = false;
-            }
-        }
-
-        // UI Logic
-        {
-            ImGui_ImplOpenGL3_NewFrame();
-            ImGui_ImplGlfw_NewFrame();
-            ImGui::NewFrame();
-
-            ImGui::Begin("Cube Stats");
-
-            ImGui::Text("Rotation: %.2f | %.2f | %.2f", cube_angles.x, cube_angles.y, cube_angles.z);
-
-            ImGui::Text("Position: %.2f | %.2f | %.2f", camera.position.x, camera.position.y, camera.position.z);
-
-            ImGui::SliderFloat("X Position", &camera.position.x, -10.0f, 10.0f);
-            ImGui::SliderFloat("Y Position", &camera.position.y, -10.0f, 10.0f);
-            ImGui::SliderFloat("Z Position", &camera.position.z, -10.0f, 10.0f);
-
-            ImGui::Separator();
-
-            ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
-            ImGui::End();
-        }
-
-        // Render Logic
-        window.clear_color(0.2f, 0.3f, 0.3f, 1.0f);
-        window.clear_buffer_bits();
-
-        // texture.set_active_texture(GL_TEXTURE0);
-        // texture.bind();
-
-        program.use();
-
-        model = transformation * rotation * scale;
-
-        view = camera.look_at();
-        projection = glm::perspective(glm::radians(camera.fov()), window.aspect_ratio(), 0.1f, 100.0f);
-
-        mvp = projection * view * model;
-
-        program.set_uniform_mat4("mvp", mvp);
-
-        mesh.bind();
-        mesh.draw();
-
-        ImGui::Render();
-        ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
-
-        // Swap the windows
-        window.swap_buffers();
-    }
-
-    // Cleanup
-    ImGui_ImplOpenGL3_Shutdown();
-    ImGui_ImplGlfw_Shutdown();
-    ImGui::DestroyContext();
-}
-
 std::vector<std::string> globe_faces
 {
     "textures/bm/right.png",
@@ -599,9 +273,9 @@ void render_sphere_via_cube_map()
     glm::mat4 scale(1.0f);
 
     glm::mat4 model(1.0f);
-    glm::mat4 view = camera.look_at();
+    glm::mat4 view = camera.view_matrix();
     //glm::mat4 projection = glm::ortho(-1.5f, 1.5f, -1.125f, 1.125f, -2.0f, 2.0f);
-    glm::mat4 projection = glm::perspective(glm::radians(camera.fov()), window.aspect_ratio(), 0.1f, 100.0f);
+    glm::mat4 projection = camera.projection_matrix(window.aspect_ratio());
 
     glm::mat4 mvp(1.0f);
 
@@ -697,7 +371,7 @@ void render_sphere_via_cube_map()
             ImGui::SliderFloat("Near", &near, 0.0f, 1.0f);
             ImGui::SliderFloat("Far", &far, 0.0f, 100.0f);
 
-            ImGui::SliderFloat("Hypotenuse", &hypotenuse, 1.0f, 2.0f);
+            ImGui::SliderFloat("Hypotenuse", &hypotenuse, 1.0f, 5.0f);
 
             ImGui::Button("Toggle me!");
 
@@ -709,7 +383,7 @@ void render_sphere_via_cube_map()
             if(button_is_toggled)
             {
                 camera.set_fov(fov);
-                projection = glm::perspective(glm::radians(camera.fov()), window.aspect_ratio(), near, far);
+                projection = camera.projection_matrix(window.aspect_ratio());
 
             }
             else
@@ -740,7 +414,7 @@ void render_sphere_via_cube_map()
 
         program.use();
 
-        view = camera.look_at();
+        view = camera.view_matrix();
         //projection = glm::ortho(-2.0f, 2.0f, -1.5f, 1.5f, -2.0f, 2.0f);
         //projection = glm::perspective(glm::radians(camera.fov()), window.aspect_ratio(), near, far);
 
